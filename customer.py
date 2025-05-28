21020c7d960c1e5d594fac044e2902fd50bdf20b
Ian Sang <sangian006@gmail.com>
2025-05-28 14:43:46 +0300

    changes to customer.py

--- customer.py
+++ customer.py
@@ -1,5 +1,6 @@
 from order import Order
 
+
 class Customer:
     all = []
 
@@ -11,7 +12,7 @@ class Customer:
     @property
     def name(self):
         return self._name
-    
+
     @name.setter
     def name(self, value):
         if not isinstance(value, str):
@@ -21,29 +22,30 @@ class Customer:
         if len(value) > 15:
             raise ValueError("Customer name cannot exceed 15 characters")
         self._name = value
-        
+
     def orders(self):
         return self._orders
-    
+
     def coffees(self):
         return list(set(order.coffee for order in self._orders))
-    
+
     def create_order(self, coffee, price):
         new_order = Order(self, coffee, price)
         return new_order
 
     def __repr__(self):
         return f"<Customer name={self.name}>"
-    
+
     @classmethod
     def most_aficionado(cls, coffee):
         if not coffee.orders():
             return None
-        
+
         spending = {}
 
+        from collections import defaultdict
+
+        spending = defaultdict(float)
         for order in coffee.orders():
-            if order.customer not in spending:
-                spending[order.customer] = 0
             spending[order.customer] += order.price
-        return max(spending.items(), key=lambda item: item[1])[0]
+            return max(spending, key=spending.get, default=None)
